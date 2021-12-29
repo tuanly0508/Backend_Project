a1c87830023c2180e4eafd78df97e6aadd4b7aed
Black_IT <tuanly0508@gmail.com>
2021-12-29 16:52:20 +0700

    n

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.post('/add', (req: Request,res: Response) => {
     )
 })
 
-//update product d
+//update product ds
 app.put('/update', (req: Request,res: Response) => {
     let i = list.findIndex(data => data.id === req.body.id)
     list[i] = req.body
